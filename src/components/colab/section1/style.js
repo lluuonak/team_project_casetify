21a13eca2fe4c15c1f6f159aef5535f8f430d41b
lluuonak <idonhava@gmail.com>
2025-08-11 16:20:28 +0900

    0811 colab 작업중
    section 1,2,3

--- src/components/colab/section1/style.js
+++ src/components/colab/section1/style.js
@@ -1,14 +1,54 @@
 import styled from 'styled-components';
 
 export const Section1Style = styled.section`
+    height: 1490px;
     .visual-banner {
-    }
-    .new-product {
-        .title {
-            h2 {
-                color: #fff;
+        position: relative;
+        .visual-image {
+            width: 100%;
+            min-width: 1920px;
+            height: 1490px;
+            /* z-index: 0; */
+            position: relative;
+            img {
             }
-            p {
+            /* 이미지 bg로 띄우면 좋은데 상태관리 style component에 적용할 수 있나 */
+            .gradient-overlay {
+                position: absolute;
+                bottom: 410px;
+                left: 0;
+                width: 100%;
+                height: 60%;
+                background: linear-gradient(transparent, rgba(0, 0, 0, 1));
+                /* pointer-events: none; */
+            }
+        }
+        .new-product {
+            width: 1200px;
+            margin: auto;
+            position: absolute;
+            z-index: 10;
+            top: 700px;
+            left: 50%;
+            transform: translateX(-50%);
+            .title {
+                z-index: 10;
+                color: #fff;
+                h2 {
+                    font-size: 56px;
+                    font-weight: 700;
+                    text-align: left;
+                    margin-bottom: 0;
+                }
+                p {
+                    font-size: 24px;
+                    font-weight: 600;
+                }
+            }
+            .new-product-list {
+                height: 484px;
+                background-color: rgb(44, 111, 255);
+                margin-top: 40px;
             }
         }
     }
