141edefb2436556c49090542335ada3c78c3ed54
이준현 <115512931+LeeJoNyun@users.noreply.github.com>
2025-08-12 15:19:31 +0900

    병합 후 배포

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,29 @@
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
-import GlobalStyle from './styled/GlobalStyle';
-import Layout from './common/Layout';
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import GlobalStyle from "./styled/GlobalStyle";
+import Layout from "./common/Layout";
 
-
-import { Main, NotFiles } from './pages';
-import Ksports from './pages/Ksports';
-
+import { Colab, Main, NotFiles } from "./pages";
+import Ksports from "./pages/Ksports";
 
 const App = () => {
-    return (
-        <>
-            <BrowserRouter>
-                <GlobalStyle />
+  return (
+    <>
+      <BrowserRouter>
+        <GlobalStyle />
 
-                <Routes>
-                    <Route path="/" element={<Layout />}>
-                        <Route index element={<Main />} />
-
-                        <Route path="Ksports" element={<Ksports />} />
-
-                        <Route path="*" element={<NotFiles />} />
-                    </Route>
-                </Routes>
-            </BrowserRouter>
-        </>
-    );
-
-    <Routes>
-        <Route path="/" element={<Layout />}>
+        <Routes>
+          <Route path="/" element={<Layout />}>
             <Route index element={<Main />} />
-            <Route path="/colab" element={<Colab />}>
-                <Route path="animation" element={<Animation />} />
-                <Route path="art" element={<Art />} />
-                {/* <Route path="characters" element={<Characters />} />
-                            <Route path="fashion" element={<Fashion />} />
-                            <Route path="movie" element={<Movie />} />
-                            <Route path="sports" element={<Sports />} /> */}
-            </Route>
+
+            <Route path="/Ksports" element={<Ksports />} />
+            <Route path="/colab" element={<Colab />} />
+
             <Route path="*" element={<NotFiles />} />
-        </Route>
-    </Routes>;
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 };
 
 export default App;
